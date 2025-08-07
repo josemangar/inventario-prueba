767ea10e727dd52f444160f46b6255b1efcaa15a
josem <josemangamer@outlook.com>
2025-08-06 18:42:27 -0600

    Configuración de Travis CI con prueba JUnit básica

--- test/pruebas/PruebaProducto.java
+++ test/pruebas/PruebaProducto.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pruebas;
 
-/**
- *
- * @author josem
- */
+import logica.Producto;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PruebaProducto {
-    
+
+    @Test
+    public void pruebaNombre() {
+        Producto p = new Producto("P01", "Lapicero", 10, 5.50, "Oficina", "Bic", "2025-10-01");
+        assertEquals("Lapicero", p.getNombre());
+    }
 }
